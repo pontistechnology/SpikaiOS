8f42a556bf9a07f5cbffab5022205aeb92e5de45
Marko Matijević <matijevicmarko33@gmail.com>
2021-10-22 16:13:46 +0200

    Created ImageButton

---
+++ Spika/Common/Views/ImageButton.swift
@@ -0,0 +1,50 @@
+//
+//  BackgroundButton.swift
+//  Spika
+//
+//  Created by Marko on 22.10.2021..
+//
+
+import UIKit
+
+class ImageButton: UIView, BaseView {
+    
+    let backgroundView = UIView()
+    let imageView = UIImageView()
+    
+    private let image: UIImage
+    private let size: CGSize
+    
+    init(image: UIImage, size: CGSize = CGSize(width: 44, height: 44)) {
+        self.image = image
+        self.size = size
+        super.init(frame: CGRect(x: .zero, y: .zero, width: size.width, height: size.height))
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubviews() {
+        addSubview(backgroundView)
+        addSubview(imageView)
+    }
+    
+    func styleSubviews() {
+        backgroundView.backgroundColor = UIColor(named: Constants.Colors.appBlueLight)
+        backgroundView.layer.cornerRadius = size.height / 2
+        backgroundView.clipsToBounds = true
+        
+        imageView.image = image.withRenderingMode(.alwaysOriginal).withTintColor(UIColor(named: Constants.Colors.appBlue)!)
+    }
+    
+    func positionSubviews() {
+        backgroundView.fillSuperview()
+        backgroundView.anchor(size: size)
+        
+        imageView.centerInSuperview()
+        imageView.anchor(size: CGSize(width: size.width * 0.4, height: size.height * 0.4))
+    }
+    
+}
