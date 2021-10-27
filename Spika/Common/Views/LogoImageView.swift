fbeaba9fd03e51da6f571f1a4d865eed6c6e9087
Marko Matijević <matijevicmarko33@gmail.com>
2021-10-27 14:54:26 +0200

    Created LogoImageView

---
+++ Spika/Common/Views/LogoImageView.swift
@@ -0,0 +1,25 @@
+//
+//  LogoImageView.swift
+//  Spika
+//
+//  Created by Marko on 27.10.2021..
+//
+
+import UIKit
+
+class LogoImageView: UIImageView {
+    
+    init(textPlaceholder: String = "") {
+        super.init(frame: .zero)
+        setupImage()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupImage() {
+        self.image = UIImage(named: "logo")
+    }
+
+}
