979c3687ed4a355045a2c05ac854b82bb3edb83d
Nikola <barbarinikola3@gmail.com>
2022-03-24 02:31:15 +0100

    better marks

--- Spika/Data/Repository/Repository.swift
+++ Spika/Data/Repository/Repository.swift
@@ -23,40 +23,63 @@ enum RepositoryType {
 
 protocol Repository {
     
+    // MARK: - Properties
+    
     var subs: Set<AnyCancellable>{ get set}
     
-    // MARK: Network
-    // MARK: - Auth
+    
+    // MARK: - NETWORKING: Auth
+    
     func authenticateUser(telephoneNumber: String, deviceId: String) -> AnyPublisher<AuthResponseModel, Error>
     func verifyCode(code: String, deviceId: String) -> AnyPublisher<AuthResponseModel, Error>
     
-    // MARK: - File upload
+    // MARK: NETWORKING: File upload
+    
     func uploadWholeFile(data: Data) -> (publisher: PassthroughSubject<UploadChunkResponseModel, Error>, totalChunksNumber: Int)
     func uploadChunk(chunk: String, offset: Int, total: Int, size: Int, mimeType: String, fileName: String, clientId: String, type: String, fileHash: String?, relationId: Int) -> AnyPublisher<UploadChunkResponseModel, Error>
     
-    // MARK: - User
+    // MARK: NETWORKING: User
+    
     func updateUser(username: String?, avatarURL: String?, telephoneNumber: String?, email: String?) -> AnyPublisher<UserResponseModel, Error>
     
-    // MARK: - Contacts
+    // MARK: NETWORKING: Contacts
+    
     func postContacts(hashes: [String]) -> AnyPublisher<ContactsResponseModel, Error>
     func getContacts(page: Int) -> AnyPublisher<ContactsResponseModel, Error>
     
-    // MARK: - Room
+    // MARK: NETWORKING: Room
+    
     func createRoom(name: String, users: [User]) -> AnyPublisher<CreateRoomResponseModel, Error>
     func checkRoom(forUserId userId: Int) -> AnyPublisher<CheckRoomResponseModel, Error>
     func createRoom(userId: Int) -> AnyPublisher<CreateRoomResponseModel, Error>
     func getAllRooms() -> AnyPublisher<GetAllRoomsResponseModel, Error>
     
-    // MARK: - Message
+    // MARK: NETWORKING: Message
+    
     func sendTextMessage(message: MessageBody, roomId: Int) -> AnyPublisher<SendMessageResponse, Error>
     
     
-    // MARK: Database
-    func saveUserInfo(user: User, device: Device)
-    func getMyUserId() -> Int
+    
+    
+    
+    
+    // MARK: - COREDATA: User
+    
     func getUsers() -> Future<[LocalUser], Error>
     func saveUser(_ user: LocalUser) -> Future<LocalUser, Error>
     func saveUsers(_ users: [LocalUser]) -> Future<[LocalUser], Error>
+    
+    // MARK: COREDATA: Messages
+    
     func saveMessage(_ message: LocalMessage) -> Future<LocalMessage, Error>
     func testnaRepo(naziv: String) -> Future<String, Error>
+
+    
+    
+    
+    
+    // MARK: - USERDEFAULTS: User
+    
+    func saveUserInfo(user: User, device: Device)
+    func getMyUserId() -> Int
 }
