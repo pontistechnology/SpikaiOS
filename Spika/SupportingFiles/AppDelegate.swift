248060ee6080fdb56c4788a5ed999d93d5cf1faf
Nikola <barbarinikola3@gmail.com>
2022-04-27 17:40:35 +0200

    push working from Firebase

--- Spika/SupportingFiles/AppDelegate.swift
+++ Spika/SupportingFiles/AppDelegate.swift
@@ -9,43 +9,17 @@ import UIKit
 import CoreData
 import AVFoundation
 import Firebase
+import FirebaseMessaging
 
  @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         
-        AVCaptureDevice.requestAccess(for: .audio) { haveMicAccess in
-            print("Access to Microphone: \(haveMicAccess)")
-        }
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
-        registerForPushNotifications()
+        configureNotifications(app: application)
         customization()
-        
         return true
-    }
-    
-    let coreDataStack = CoreDataStack()
-    
-    func allroomsprinter() {
-        let fr = RoomEntity.fetchRequest()
-        fr.predicate = NSPredicate(format: "type == 'private' AND ANY users.userId == 12")
-        let aa = try! coreDataStack.mainMOC.fetch(fr)
-        print(aa.count)
-        for a in aa {
-            print("Count of users: ", a.users!.count)
-            for user in a.users! {
-                print("Begi user: ", (user as! RoomUserEntity).user!.displayName!)
-            }
-        }
-    }
-    
-    func test() {
-        // only for debug, remove later
-        // print("type is: ", MessageType(rawValue: "textf"))
-         UserDefaults.standard.set("STx4njvXxPVOgxR7", forKey: Constants.UserDefaults.accessToken)
     }
     
     func customization() {
@@ -59,67 +33,53 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             [   NSAttributedString.Key.font : font,
             ], for: .disabled)
     }
+}
+
+
+// MARK: Push notifications
+
+extension AppDelegate: MessagingDelegate, UNUserNotificationCenterDelegate {
     
-    func registerForPushNotifications() {
-        UNUserNotificationCenter.current()
-          .requestAuthorization(
-            options: [.alert, .sound, .badge]) { [weak self] granted, _ in
-            print("Permission granted: \(granted)")
-            guard granted else { return }
-            self?.getNotificationSettings()
-          }
-    }
-    
-    func getNotificationSettings() {
-        UNUserNotificationCenter.current().getNotificationSettings { settings in
-            print("Notification settings: \(settings)")
-          guard settings.authorizationStatus == .authorized else { return }
-          DispatchQueue.main.async {
-            UIApplication.shared.registerForRemoteNotifications()
-          }
-      }
-    }
-    
-    func application(
-      _ application: UIApplication,
-      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-    ) {
-      let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
-      let token = tokenParts.joined()
-      print("Push Token: \(token)")
-        UserDefaults.standard.set(token, forKey: Constants.UserDefaults.pushToken)
-    }
-    
-    func application(
-      _ application: UIApplication,
-      didFailToRegisterForRemoteNotificationsWithError error: Error
-    ) {
-        print("Failed to register: \(error)")
-    }
-    
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        
-        print("PUSH: ", userInfo)
-        if let aps = userInfo["aps"] as? [String: Any], let alert = aps["alert"] as? [String: Any] {
-            let title = alert["title"] as? String ?? ""
-            let body = alert["body"] as? String ?? ""
-            
-            
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        messaging.token { token, _ in
+            guard let token = token else { return }
+            print("New Push fcmToken: ", token)
+            UserDefaults.standard.set(token, forKey: Constants.UserDefaults.pushToken)
         }
     }
     
-    // MARK: UISceneSession Lifecycle
+    func configureNotifications(app: UIApplication) {
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, _ in
+            guard success else { return }
+            print("APNs registred.")
+        }
+        app.registerForRemoteNotifications()
+    }
+}
+
+// MARK: CoreData, testing, TODO: delete later
+
+extension AppDelegate {
     
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func test() {
+         UserDefaults.standard.set("STx4njvXxPVOgxR7", forKey: Constants.UserDefaults.accessToken)
     }
     
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func allroomsprinter() {
+        let coreDataStack = CoreDataStack()
+
+        let fr = RoomEntity.fetchRequest()
+        fr.predicate = NSPredicate(format: "type == 'private' AND ANY users.userId == 12")
+        let aa = try! coreDataStack.mainMOC.fetch(fr)
+        print(aa.count)
+        for a in aa {
+            print("Count of users: ", a.users!.count)
+            for user in a.users! {
+                print("Begi user: ", (user as! RoomUserEntity).user!.displayName!)
+            }
+        }
     }
 }
 
