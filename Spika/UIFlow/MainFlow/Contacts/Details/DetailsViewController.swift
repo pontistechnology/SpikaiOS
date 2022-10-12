12b8ab8deb0ed8584ee6d35200b4e1c0bb6e2f78
Nikola <barbarinikola3@gmail.com>
2022-10-12 04:09:50 +0200

    displayName on details screen

--- Spika/UIFlow/MainFlow/Contacts/Details/DetailsViewController.swift
+++ Spika/UIFlow/MainFlow/Contacts/Details/DetailsViewController.swift
@@ -66,7 +66,7 @@ class DetailsViewController: BaseViewController {
         
         viewModel.userSubject.receive(on: DispatchQueue.main).sink { [weak self] user in
             guard let self = self else { return }
-            self.detailsView.contentView.nameLabel.text = user.displayName
+            self.detailsView.contentView.nameLabel.text = user.getDisplayName()
             let url = URL(string: user.getAvatarUrl() ?? "")
             self.detailsView.contentView.profilePhoto.kf.setImage(with: url, placeholder: UIImage(safeImage: .userImage))
         }.store(in: &subscriptions)
