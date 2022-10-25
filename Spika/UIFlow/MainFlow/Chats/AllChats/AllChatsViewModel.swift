0a8e9a7f924cf66e5ed15db3ec03f07746c64c7e
Nikola <barbarinikola3@gmail.com>
2022-10-25 08:31:38 +0200

    getMyUserId moved to BaseViewModel, getAllRooms removed

--- Spika/UIFlow/MainFlow/Chats/AllChats/AllChatsViewModel.swift
+++ Spika/UIFlow/MainFlow/Chats/AllChats/AllChatsViewModel.swift
@@ -21,25 +21,4 @@ class AllChatsViewModel: BaseViewModel {
     func presentCurrentChatScreen(room: Room) {
         getAppCoordinator()?.presentCurrentChatScreen(room: room)
     }
-    
-    func getAllRooms() {
-        repository.getAllRooms().sink { [weak self] completion in
-            guard let _ = self else { return }
-            print("get all rooms: ", completion)
-        } receiveValue: { [weak self] response in
-            guard let self = self else { return }
-            guard let rooms = response.data?.list else { return }
-            
-            self.repository.saveLocalRooms(rooms: rooms).sink { c in
-                print("Save room allCV: ", c)
-            } receiveValue: { rooms in
-                
-            }.store(in: &self.subscriptions)
-            print("rooms server: ", rooms.count)
-        }.store(in: &subscriptions)
-    }
-    
-    func getMyUserId() -> Int64 {
-        return repository.getMyUserId()
-    }
 }
