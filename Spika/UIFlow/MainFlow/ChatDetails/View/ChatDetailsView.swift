6165281d23e807e0ea65893321524cf59919d1e4
Vedran Vugrin <vedran.vugrin@clover.studio>
2022-11-11 09:33:54 +0100

    Added chat content view

--- Spika/UIFlow/MainFlow/ChatDetails/View/ChatDetailsView.swift
+++ Spika/UIFlow/MainFlow/ChatDetails/View/ChatDetailsView.swift
@@ -15,44 +15,7 @@ class ChatDetailsView: UIView, BaseView {
         return view
     } ()
     
-    lazy var  mainHolderView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    } ()
-    
-    let chatImage = UIImageView()
-    let chatName = CustomLabel(text: "Group", fontName: .MontserratSemiBold)
-    
-    let sharedMediaOptionButton = NavView(text: "Shared Media, Links and Docs")
-    let chatSearchOptionButton = NavView(text: "Chat search")
-    let callHistoryOptionButton = NavView(text: "Call history")
-    
-    let notesOptionButton = NavView(text: "Notes")
-    let favoriteMessagesOptionButton = NavView(text: "Favorites")
-    
-    let pinChatSwitchView = SwitchView(text: "Pin chat")
-    let muteSwitchView = SwitchView(text: "Mute")
-    let blockLabel = CustomLabel(text: "Block", textSize: 14, textColor: .appRed)
-    let reportLabel = CustomLabel(text: "Report", textSize: 14, textColor: .appRed)
-    
-    lazy var optionButtonsStackView: UIStackView = {
-       let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.distribution = .fill
-        return stackView
-    } ()
-    
-    lazy var labelStackView: UIStackView = {
-       let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.distribution = .fillEqually
-        stackView.layoutMargins = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
-        stackView.isLayoutMarginsRelativeArrangement = true
-        return stackView
-    } ()
+    let contentView = ChatContentView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -66,51 +29,19 @@ class ChatDetailsView: UIView, BaseView {
     
     func addSubviews() {
         self.addSubview(self.scrollView)
-        self.scrollView.addSubview(self.mainHolderView)
-        
-        self.mainHolderView.addSubview(chatImage)
-        self.mainHolderView.addSubview(chatName)
-        self.mainHolderView.addSubview(optionButtonsStackView)
-        
-        optionButtonsStackView.addArrangedSubview(sharedMediaOptionButton)
-        optionButtonsStackView.addArrangedSubview(chatSearchOptionButton)
-        optionButtonsStackView.addArrangedSubview(callHistoryOptionButton)
-        
-        optionButtonsStackView.addArrangedSubview(notesOptionButton)
-        optionButtonsStackView.addArrangedSubview(favoriteMessagesOptionButton)
-        
-        optionButtonsStackView.addArrangedSubview(pinChatSwitchView)
-        optionButtonsStackView.addArrangedSubview(muteSwitchView)
-        
-        optionButtonsStackView.addArrangedSubview(self.labelStackView)
-        labelStackView.addArrangedSubview(blockLabel)
-        labelStackView.addArrangedSubview(reportLabel)
+        self.scrollView.addSubview(self.contentView)
     }
     
-    func styleSubviews() {
-        chatImage.image = UIImage(safeImage: .testImage)
-        chatImage.layer.cornerRadius = 50
-        chatImage.contentMode = .scaleAspectFill
-        chatImage.clipsToBounds = true
-    }
+    func styleSubviews() {}
     
     func positionSubviews() {
         self.scrollView.fillSuperview()
-        self.mainHolderView.anchor(top: self.scrollView.contentLayoutGuide.topAnchor,
-                                   leading: self.scrollView.contentLayoutGuide.leadingAnchor,
-                                   bottom: self.scrollView.contentLayoutGuide.bottomAnchor,
-                                   trailing: self.scrollView.contentLayoutGuide.trailingAnchor)
-        self.mainHolderView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
         
-        chatImage.anchor(top: self.mainHolderView.topAnchor, padding: UIEdgeInsets(top: 16, left: 0, bottom: 0, right: 0), size: CGSize(width: 100, height: 100))
-        chatImage.centerXToSuperview()
-        
-        chatName.anchor(top: chatImage.bottomAnchor, padding: UIEdgeInsets(top: 16, left: 0, bottom: 0, right: 0))
-        chatName.centerXToSuperview()
-        
-        optionButtonsStackView.anchor(top: chatName.bottomAnchor, leading: self.mainHolderView.leadingAnchor, bottom: self.mainHolderView.bottomAnchor, trailing: self.mainHolderView.trailingAnchor, padding: UIEdgeInsets(top: 24, left: 0, bottom: 0, right: 0))
-        
-        blockLabel.constrainHeight(80)
+        self.contentView.anchor(top: self.scrollView.contentLayoutGuide.topAnchor,
+                                leading: self.scrollView.contentLayoutGuide.leadingAnchor,
+                                bottom: self.scrollView.contentLayoutGuide.bottomAnchor,
+                                trailing: self.scrollView.contentLayoutGuide.trailingAnchor)
+        self.contentView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
     }
     
 }
