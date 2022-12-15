728259aaab03e83a3c553b7a721903b98ad4aa24
Nikola <barbarinikola3@gmail.com>
2022-12-15 08:48:28 +0100

    image ratio

--- Spika/Common/Views/UITableViewCells/MessageTableViewCells/Views/MessageImageView.swift
+++ Spika/Common/Views/UITableViewCells/MessageTableViewCells/Views/MessageImageView.swift
@@ -10,6 +10,8 @@ import UIKit
 class MessageImageView: UIView {
     
     private let imageView = UIImageView()
+    private var imageWidthConstraint: NSLayoutConstraint?
+    private var imageHeightConstraint: NSLayoutConstraint?
     
     init() {
         super.init(frame: .zero)
@@ -36,17 +38,38 @@ extension MessageImageView: BaseView {
     
     func positionSubviews() {
         imageView.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: UIEdgeInsets(top: 4, left: 4, bottom: 4, right: 4))
-        imageView.constrainWidth(256)
-        imageView.constrainHeight(256)
+        imageWidthConstraint = imageView.widthAnchor.constraint(equalToConstant: 246)
+        imageHeightConstraint = imageView.heightAnchor.constraint(equalToConstant: 246)
+        imageWidthConstraint?.isActive = true
+        imageHeightConstraint?.isActive = true
+        // TODO: constraint priority warning bug
     }
 }
 
 extension MessageImageView {
-    func setImage(url: URL?) {
+    func setImage(url: URL?, as ratio: ImageRatio) {
+        
+        switch ratio {
+        case .portrait:
+            imageWidthConstraint?.constant = 140
+            imageHeightConstraint?.constant = 246
+        case .landscape:
+            imageWidthConstraint?.constant = 240
+            imageHeightConstraint?.constant = 114
+        case .square:
+            imageWidthConstraint?.constant = 246
+            imageHeightConstraint?.constant = 246
+        }
         imageView.kf.setImage(with: url, placeholder: UIImage(systemName: "arrow.counterclockwise")?.withTintColor(.gray, renderingMode: .alwaysOriginal)) // TODO: change image
     }
     
     func reset() {
         imageView.image = nil
     }
+}
+
+enum ImageRatio {
+    case portrait
+    case landscape
+    case square
 }
