0b80ee34179efea06821e4bff5b3d7869a0ffd3d
Vedran Vugrin <vedran.vugrin@clover.studio>
2023-06-23 13:55:55 +0200

    Added new target, added new info.plist, added new google service info plist for messanger app.

---
+++ Spika/Common/Constants/ConstantsMessenger.swift
@@ -0,0 +1,22 @@
+//
+//  ConstantsMessenger.swift
+//  Spika
+//
+//  Created by Vedran Vugrin on 23.06.2023..
+//
+
+import Foundation
+
+public class Constants {
+    
+    public class Networking {
+        static let baseUrl = "https://dev3.spika.chat/"
+        static let appGroupName = "group.studio.clover.Spika.groupSpikaDev"
+        static let userIds = "userIds"
+    }
+    
+    public class CABasicAnimations {
+        static let circularProgressStroke = "circularProgressStroke"
+    }
+    
+}
