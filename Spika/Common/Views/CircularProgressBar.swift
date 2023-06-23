2371f5f99927f7479a8823be73e8dc427e019c95
Nikola <barbarinikola3@gmail.com>
2023-06-23 07:55:47 +0200

    added label to progress bar

--- Spika/Common/Views/CircularProgressBar.swift
+++ Spika/Common/Views/CircularProgressBar.swift
@@ -15,6 +15,7 @@ class CircularProgressBar: UIView, BaseView {
     private let lineWidth: CGFloat = 5.0
     private let backgroundView = UIView()
     private let containerView  = UIView()
+    private let label = CustomLabel(text: "44%")
     
     init(spinnerWidth: CGFloat) {
         self.width = spinnerWidth
@@ -29,6 +30,7 @@ class CircularProgressBar: UIView, BaseView {
     func addSubviews() {
         addSubview(backgroundView)
         backgroundView.addSubview(containerView)
+        containerView.addSubview(label)
     }
     
     func styleSubviews() {
@@ -55,14 +57,18 @@ class CircularProgressBar: UIView, BaseView {
         containerView.constrainWidth(width)
         containerView.constrainHeight(width)
         containerView.centerInSuperview()
+        label.centerXToSuperview()
+        label.centerYToSuperview(offset: 20)
     }
     
     func setProgress(to value: CGFloat) {
         containerView.layer.removeAllAnimations()
         shapeLayer.strokeEnd = value
+        label.text = "\(value * 100) %"
     }
     
-    func startSpinning() {
+    func startSpinning(with text: String? = nil) {
+        label.text = text
         shapeLayer.strokeEnd = 0.75
         let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
         rotationAnimation.fromValue = 0.0
