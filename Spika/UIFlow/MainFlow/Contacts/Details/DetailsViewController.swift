68605e39e5ba6648f197a46c0b2de8516356b212
Nikola <barbarinikola3@gmail.com>
2023-07-03 07:32:02 +0200

    copy and add to contacts funcionality on details screen

--- Spika/UIFlow/MainFlow/Contacts/Details/DetailsViewController.swift
+++ Spika/UIFlow/MainFlow/Contacts/Details/DetailsViewController.swift
@@ -69,11 +69,47 @@ class DetailsViewController: BaseViewController {
             let url = user.avatarFileId?.fullFilePathFromId()
             self.detailsView.contentView.profilePhoto.kf.setImage(with: url, placeholder: UIImage(safeImage: .userImage))
         }.store(in: &subscriptions)
+        
+        self.detailsView.contentView.phoneNumberLabel.text = viewModel.getPhoneNumberText()
+        self.detailsView.contentView.phoneNumberLabel.tap().sink { [weak self] _ in
+            self?.phoneNumberLabelTapped()
+        }.store(in: &subscriptions)
     }
     
     
     deinit {
 //        print("DetailsViewController deinit")
     }
+}
+
+extension DetailsViewController {
+    func phoneNumberLabelTapped() {
+        viewModel
+            .getAppCoordinator()?
+            .showAlert(actions: [.regular(title: .getStringFor(.copy)),
+                                 .regular(title: .getStringFor(.addToContacts))
+            ], cancelText: .getStringFor(.cancel))
+            .sink(receiveValue: { [weak self] tappedIndex in
+                switch tappedIndex {
+                case 0:
+                    self?.copyPhoneNumber()
+                case 1:
+                    self?.addToContacts()
+                default:
+                    break
+                }
+            }).store(in: &subscriptions)
+    }
     
+    func copyPhoneNumber() {
+        UIPasteboard.general.string = self.detailsView.contentView.phoneNumberLabel.text
+        viewModel.showOneSecAlert(type: .copy)
+    }
+    
+    func addToContacts() {
+        guard let phoneNumber = self.detailsView.contentView.phoneNumberLabel.text,
+              let name = self.detailsView.contentView.nameLabel.text
+        else { return }
+        viewModel.presentAddToContactsScreen(name: name, number: phoneNumber)
+    }
 }
