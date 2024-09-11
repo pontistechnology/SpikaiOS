869628d2a160850cb2462d22d947be6f07a5422b
Nikola <barbarinikola3@gmail.com>
2024-09-11 17:38:02 +0200

    v3 bug_4 fix

--- Spika/UIFlow/MainFlow/Chats/AllChats/View/AllChatsView.swift
+++ Spika/UIFlow/MainFlow/Chats/AllChats/View/AllChatsView.swift
@@ -62,7 +62,7 @@ class AllChatsView: UIView, BaseView {
         chatLabel.anchor(top: topAnchor, leading: leadingAnchor, padding: UIEdgeInsets(top: 10, left: 20, bottom: 0, right: 0))
         infoLabel.anchor(top: chatLabel.bottomAnchor, leading: chatLabel.leadingAnchor, padding: .init(top: 4, left: 0, bottom: 0, right: 0))
         
-        newChatButton.anchor(leading: leadingAnchor, bottom: bottomAnchor, padding: UIEdgeInsets(top: 0, left: 20, bottom: HomeTabBar.tabBarHeight, right: 0), size: CGSize(width: 52, height: 52))
+        newChatButton.anchor(bottom: bottomAnchor, trailing: trailingAnchor, padding: UIEdgeInsets(top: 0, left: 0, bottom: HomeTabBar.tabBarHeight, right: 20), size: CGSize(width: 52, height: 52))
         searchBar.anchor(top: chatLabel.bottomAnchor, leading: leadingAnchor, trailing: trailingAnchor, padding: UIEdgeInsets(top: 20, left: 12, bottom: 0, right: 12))
         roomsTableView.anchor(top: searchBar.bottomAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: UIEdgeInsets(top: 10, left: 0, bottom: HomeTabBar.tabBarHeight, right: 0))
         searchedMessagesTableView.anchor(top: roomsTableView.topAnchor, leading: roomsTableView.leadingAnchor, bottom: roomsTableView.bottomAnchor, trailing: roomsTableView.trailingAnchor, padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0))
